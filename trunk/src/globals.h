52b4828979f54251db35f299ee0e79b5f1f914b2
ben.foote@gmail.com <ben.foote@gmail.com@b77f994c-2a03-fbd4-c015-4b865d4f5af1>
2013-08-14 14:16:20 +0000

    Partially redid the item defines to make droppables Vs. inventory items different from one another. It's not complete yet, I need to think of more items...

--- trunk/src/globals.h
+++ trunk/src/globals.h
@@ -4,17 +4,28 @@
 #include "object.h"
 
 
-// Item related defines, a total of 16 in game items.
-#define HEALTH_POTION     0x0001
-#define HASTE_POTION      0x0002
-#define INVINCIBLE_POTION 0x0004
-#define STRENGTH_POTION   0x0008
-#define LONGSWORD         0x0010
-#define LIGHTSWORD        0x0020
-#define CLOTH_ARMOR       0x0040
-#define LEATHER_ARMOR     0x0080
-#define GOLD              0x0100
-#define KEY               0x0200
+// Droppable items that the player can pick up to provide 
+// temporary effects.
+#define HEALTH_POTION           1U
+#define INVINCIBILITY_POTION    2U
+#define INVISIBILITY_POTION     3U
+#define GOLD                    4U
+#define BOMB                    5U
+
+
+// Plyaer inventory related defines, a max of 16 in game items.
+#define CLOTH_ARMOR       0x0001
+#define LEATHER_ARMOR     0x0002
+#define CHAIN_ARMOR       0x0004
+#define PLATE_ARMOR       0x0008
+#define DAGGER            0x0010
+#define STEEL_SWORD       0x0020
+#define LONGSWORD         0x0040
+#define LIGHTSWORD        0x0080
+//#define XXX               0x0100
+//#define XXX               0x0200
+//#define XXX               0x0400
+//#define XXX               0x0800
 
 // Defines for our tile types.
 #define EMPTY	    0U
